2c453e21f980d7b2ba244a52fa29bdd1b68310d1
chen8913w <chen8913w@gmail>
2014-11-06 15:34:43 -0800

    Test

---
+++ src/PreorderRecoveryTest.java
@@ -0,0 +1,27 @@
+import javax.swing.tree.TreeNode;
+
+public class PreorderRecoveryTest {
+
+	private static void pre_traverse(TreeNode pTree, StringBuffer sb) {
+
+		if (pTree != null) {
+			// sb.append(pTree.val);
+			System.out.print(pTree.val);
+			if (pTree.left != null)
+				pre_traverse(pTree.left, sb);
+			if (pTree.right != null)
+				pre_traverse(pTree.right, sb);
+		}
+	}
+
+	public static void main(String[] args) {
+		PreorderRecovery test = new PreorderRecovery();
+
+		String input = "ABD##EF##G##C##";
+		TreeNode newTree = test.preorderRecovery(input);
+		StringBuffer sb = new StringBuffer();
+		pre_traverse(newTree, sb);
+		System.out.println(sb.toString());
+
+	}
+}
