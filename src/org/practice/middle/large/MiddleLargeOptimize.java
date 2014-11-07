0014aa53a3f957fb478531677b8dbe5d5c538549
chen8913w <chen8913w@gmail>
2014-11-06 17:26:54 -0800

    Modification

--- src/org/practice/middle/large/MiddleLargeOptimize.java
+++ src/org/practice/middle/large/MiddleLargeOptimize.java
@@ -1,18 +1,19 @@
 package org.practice.middle.large;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class MiddleLargeOptimize {
-	public int middleLargeOptimize(int[] A){
-		int result=0;
-		for(int i=1;i<A.length;i++){
-			int left=0,right=0;
-			for(int j=0;j<A.length;j++){
-				if(j==i) continue;
-				if(A[j]<A[i]&&j<i) left++;
-				if(A[j]<A[i]&&j>i) right++;
+	public int middleLargeOptimize(int[] A) {
+		int result = 0;
+		for (int i = 1; i < A.length; i++) {
+			int left = 0, right = 0;
+			for (int j = 0; j < A.length; j++) {
+				if (j == i)
+					continue;
+				if (A[j] < A[i] && j < i)
+					left++;
+				if (A[j] < A[i] && j > i)
+					right++;
 			}
-			result+=left*right;
+			result += left * right;
 		}
 		return result;
 	}
